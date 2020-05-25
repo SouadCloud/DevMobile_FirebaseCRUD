dd137d0dab00e1c32fb331e8e43bd64b01bac89f
SouadCloud <etudiant.benjemaa.souad@uvt.tn>
2020-05-25 23:39:22 +0100

    Storage commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { AuthenticationService } from '../services/authentication.service';
-import { NavController } from '@ionic/angular';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms'; // validation
+import { AuthenticationService } from '../services/authentication.service'; //authenticate
+import { NavController } from '@ionic/angular'; //navigation
+
+import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage'; //storage module
+import { AngularFirestore } from '@angular/fire/firestore';
 interface NoteData {
   Title: string;
   Content: string;
 }
+export interface Image {
+  id: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+//module import image / fire storage
+  url: any;
+    newImage: Image = {
+      id: this.afs.createId(), image: ''};
+    loading: boolean = false;
 
   noteList =[];
   noteData: NoteData;
@@ -22,7 +35,7 @@ export class HomePage implements OnInit {
   noteForm : FormGroup;
   userEmail : string; //authentication
 
-  constructor(private firebaseService : FirebaseService,private navCtrl : NavController, private authService : AuthenticationService) {
+  constructor(private afs : AngularFirestore,private storage : AngularFireStorage, private firebaseService : FirebaseService,private navCtrl : NavController, private authService : AuthenticationService) {
     this.noteData = {} as NoteData;
   }
   ngOnInit(){
@@ -100,4 +113,49 @@ export class HomePage implements OnInit {
         console.log(error);
       })
   }
+
+  //fire storage : uploat image
+  uploadImage(event) {
+    this.loading = true;
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+     
+      reader.readAsDataURL(event.target.files[0]);
+      // For Preview Of Image
+      reader.onload = (e:any) => { // called once readAsDataURL is completed
+        this.url = e.target.result;
+      
+        // For Uploading Image To Firebase
+        const fileraw = event.target.files[0];
+        console.log(fileraw)
+        const filePath = '/Image/' + this.newImage.id + '/' + 'Image' + (Math.floor(1000 + Math.random() * 9000) + 1);
+        const result = this.SaveImageRef(filePath, fileraw);
+        const ref = result.ref;
+        result.task.then(a => {
+          ref.getDownloadURL().subscribe(a => {
+            console.log(a);
+            
+            this.newImage.image = a;
+            this.loading = false;
+          });
+
+          this.afs.collection('Image').doc(this.newImage.id).set(this.newImage);
+        });
+      }, error => {
+        alert("Error");
+      }
+
+    }
+  }
+
+
+
+  SaveImageRef(filePath, file) {
+
+    return {
+      task: this.storage.upload(filePath, file)
+      , ref: this.storage.ref(filePath)
+    };
+  }
+
 }
